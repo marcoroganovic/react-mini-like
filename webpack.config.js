c62c7b91982873e14f2bd10bcd8cfc12f702234a
Marco Roganovic <marcoroganovic@gmail.com>
2017-11-24 19:32:31 +0100

    add babel loaders in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,24 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/main.js",
+
   output: {
     filename: "react-like.js",
     path: path.resolve(__dirname, "dist")
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loaded: "babel-loader",
+          options: {
+            presets: ["babel-preset-env"]
+          }
+        }
+      }
+    ]
   }
 }
